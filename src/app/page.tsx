fa1408ee2186748b797aeb2dda6aba60d8a0e1a8
KatiaKalynchuk <katerynakalynchuk@gmail.com>
2024-12-22 20:17:57 +0200

    feat: connect supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos} = await supabase.from('todo').select();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -48,6 +55,11 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <ul>
+          {todos?.map((todo) => (
+            <li>{todo.title}</li>
+          ))}
+        </ul>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
